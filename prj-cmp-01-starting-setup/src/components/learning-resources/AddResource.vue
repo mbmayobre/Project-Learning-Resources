06928bc27308ad0af1c538015915fa15851e8b79
Mike <miguel80nine@yahoo.com>
2020-11-11 22:52:45 -0500

    created BaseDialog.vue

--- prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
+++ prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -1,38 +1,72 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitData">
-            <div class="form-control">
-                <label for="title">Title</label>
-                <input id="title" name="title" type="text" ref="titleInput" />
-            </div>
-            <div class="form-control">
-                <label for="description">Description</label>
-                <textarea id="description" name="description" rows="3" ref="descInput"></textarea>
-            </div>
-            <div class="form-control">
-                <label for="link">Link</label>
-                <input id="link" name="link" type="url" ref="linkInput" />
-            </div>
-            <div>
-                <base-button type="submit">Add Resource</base-button>
-            </div>
-        </form>
-    </base-card>
+    <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+        <template v-slot:default>
+            <p>Unfortunately, at least one input value is invalid.</p>
+            <p>Please check all inputs and make sure you enter at least a few characters into each input field.</p>
+        </template>
+        <template v-slot:actions>
+            <base-button @click="confirmError">Okay</base-button>
+        </template>
+    </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          rows="3"
+          ref="descInput"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="url" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
 </template>
 
 <script>
+import BaseButton from '../UI/BaseButton.vue';
+import BaseDialog from '../UI/BaseDialog.vue';
 export default {
-    inject: ['addResource'],
-    methods: {
-        submitData() {
-            const enteredTitle = this.$refs.titleInput.value;
-            const enteredDescription = this.$refs.descInput.value;
-            const enteredUrl = this.$refs.linkInput.value;
+  components: { BaseDialog, BaseButton },
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descInput.value;
+      const enteredUrl = this.$refs.linkInput.value;
 
-            this.addResource(enteredTitle, enteredDescription, enteredUrl);
-        }
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
+      this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+    confirmError() {
+        this.inputIsInvalid = false;
     }
-}
+  },
+};
 </script>
 
 <style scoped>
--- prj-cmp-01-starting-setup/src/main.js
+++ prj-cmp-01-starting-setup/src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
+import BaseDialog from './components/UI/BaseDialog.vue';
 
 const app = createApp(App)
 
@@ -10,5 +11,6 @@ const app = createApp(App)
 // they can be used anywhere in the app
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
+app.component('base-dialog', BaseDialog);
 
 app.mount('#app');
